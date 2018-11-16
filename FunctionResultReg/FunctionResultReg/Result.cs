fdb8285a5b71dd7ea0bb54a9d7758b27b0333e64
ErikM <erik.martinsen@makingwaves.com>
2018-11-16 10:42:23 +0100

    temp1

--- FunctionResultReg/FunctionResultReg/Result.cs
+++ FunctionResultReg/FunctionResultReg/Result.cs
@@ -11,25 +11,19 @@ namespace FunctionResultReg
     public static class Result
     {
         [FunctionName("Result")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+            string name = data?.name;
 
-            if (name == null)
-            {
-                // Get request body
-                dynamic data = await req.Content.ReadAsAsync<object>();
-                name = data?.name;
-            }
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+            var res = string.IsNullOrWhiteSpace(name) 
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body") 
                 : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+
+            return res;
         }
     }
 }
