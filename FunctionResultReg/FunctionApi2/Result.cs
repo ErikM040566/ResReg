05db0bb973586969f9b391ff755e49bec8b3f088
ErikM <erik.martinsen@makingwaves.com>
2018-11-19 08:07:20 +0100

    new start with core

--- FunctionResultReg/FunctionApi2/Result.cs
+++ FunctionResultReg/FunctionApi2/Result.cs
@@ -13,21 +13,20 @@ namespace FunctionResultReg.FunctionApi
     public static class Result
     {
         [FunctionName("Result")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            if (data != null)
+            {
+                return (ActionResult) new OkObjectResult(data);
+            }
+            else
+            {
+                return new BadRequestObjectResult("Please pass json data in the request body");
+            }
         }
     }
 }
